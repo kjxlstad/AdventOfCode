71bf8d0c9049f5c8114e041385db88cabc0fb519
kjxlstad <jonathan.kjolstad@gmail.com>
2021-12-03 19:48:04 +0100

    procedural approach, not sure if better, less readable

--- day02.py
+++ day02.py
@@ -1,16 +1,22 @@
+from functools import reduce
+
 data = [line.split(" ") for line in open("data/day02.in", "r").readlines()]
 cmds = [(cmd, int(mag)) for cmd, mag in data]
 
-# Part 1 & 2
-pos = depth = aim = 0
-for cmd, mag in cmds:
-    if cmd == "forward":
-        pos += mag
-        depth += mag * aim
-    elif cmd == "down":
-        aim += mag
-    else:
-        aim -= mag
 
+# Part 1 & 2
+actions = {
+    "up":      lambda a, b: (a[0],     a[1],            a[2] - b),
+    "down":    lambda a, b: (a[0],     a[1],            a[2] + b),
+    "forward": lambda a, b: (a[0] + b, a[1] + a[2] * b, a[2]    ),
+}
+
+
+def move(state, cmd):
+    cmd, mag = cmd
+    return actions[cmd](state, mag)
+
+
+pos, depth, aim = reduce(move, cmds, (0, 0, 0))
 print(pos * aim)
 print(pos * depth)
