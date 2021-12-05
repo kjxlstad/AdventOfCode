2a135b89e76916d2026208eb903f30293d385f3a
Jonathan <jonathan.kjolstad@gmail.com>
2021-12-05 20:55:57 +0100

    Generalized line cover  to single case

--- day05/solution.py
+++ day05/solution.py
@@ -1,46 +1,22 @@
 from functools import reduce
+from itertools import zip_longest
 
 
-def vertical_cover(line):
-    x = line[0][0]
-    ys = list(zip(*line))[1]
-
-    return ((x, y) for y in range(min(ys), max(ys) + 1))
-
-
-def horizontal_cover(line):
-    def flip(line):
-        return [p[::-1] for p in line]
-
-    return flip(vertical_cover(flip(line)))
-
-
-def diagonal_cover(line):
-    dirs = [1 if line[0][ax] < line[1][ax] else -1 for ax in {0, 1}]
+def cover(line, include_diag=False):
+    dirs = [1 if line[0][ax] <= line[1][ax] else -1 for ax in {0, 1}]
 
     x_range, y_range = [
         range(ax[0], ax[1] + dirs[i], dirs[i]) for i, ax in enumerate(zip(*line))
     ]
-
-    return (p for p in zip(x_range, y_range))
-
-
-def cover(line, include_diag=False):
-    # Vertical
-    if line[0][0] == line[1][0]:
-        return vertical_cover(line)
-
-    # Horizontal
-    elif line[0][1] == line[1][1]:
-        return horizontal_cover(line)
-
-    # Diagonal
-    elif include_diag:
-        return diagonal_cover(line)
-
-    # Bin diagonals
-    else:
+    
+    # In case of horizontal or vertical pad zip with the stationary value
+    fill = list(x_range)[0] if len(x_range) == 1 else list(y_range)[0]
+    diagonal = len(x_range) != 1 and len(y_range) != 1
+    
+    if diagonal and not include_diag:
         return ()
+    
+    return (p for p in zip_longest(x_range, y_range, fillvalue=fill))
 
 
 def count_unique(acc, new):
