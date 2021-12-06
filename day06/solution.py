7b8ffd5518cf85c9fbfe25568403f0ca6504cc39
kjxlstad <jonathan.kjolstad@gmail.com>
2021-12-06 07:04:45 +0100

    Probably a more proper use of plural form of fish???

--- day06/solution.py
+++ day06/solution.py
@@ -1,11 +1,11 @@
 def propagate(fishes):
     # Aging
-    aged_fishes = {age: fishes[(age + 1) % 9] for age in range(9)}
+    aged_fish = {age: fishes[(age + 1) % 9] for age in range(9)}
 
     # Spawning
-    aged_fishes[6] += fishes[0]
+    aged_fish[6] += fishes[0]
 
-    return aged_fishes
+    return aged_fish
 
 
 def simulate(fishes, days_left):
@@ -16,9 +16,9 @@ def simulate(fishes, days_left):
 
 
 if __name__ == "__main__":
-    fishes = [int(n) for n in open("data.in", "r").read().split(",")]
+    fish = [int(n) for n in open("data.in", "r").read().split(",")]
 
-    fish_ages = {age: fishes.count(age) for age in range(9)}
+    fish_ages = {age: fish.count(age) for age in range(9)}
 
     print(simulate(fish_ages, 80))
     print(simulate(fish_ages, 256))
