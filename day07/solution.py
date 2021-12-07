f2474115ab1efd58d1910f374f3c9ea0a1e4634c
kjxlstad <jonathan.kjolstad@gmail.com>
2021-12-07 06:56:17 +0100

    Sum over range replaced by sum of arthimetic progression closed formula

--- day07/solution.py
+++ day07/solution.py
@@ -3,7 +3,8 @@ def constant_cost(a, b):
 
 
 def incremental_cost(a, b):
-    return sum(range(1, constant_cost(a, b) + 1))
+    diff = constant_cost(a, b)
+    return diff * (diff + 1) // 2
 
 
 def fuel_costs(crabs, target, cost_func):
@@ -23,6 +24,5 @@ if __name__ == "__main__":
     # Collect crabs with common position, reduces overhead with ~40%
     crab_positions = {pos: crabs.count(pos) for pos in crabs}
 
-    # Very inefficient as of now, part 2 takes a few seconds to run
     print(minimum_cost(crab_positions, constant_cost))
     print(minimum_cost(crab_positions, incremental_cost))
