9dafcc0c7453d771d7e3e4ee3b787277e5562ef7
kjxlstad <jonathan.kjolstad@gmail.com>
2021-12-07 09:44:13 +0100

    Utilize iterators all way, slight rewritings

--- day07/solution.py
+++ day07/solution.py
@@ -8,21 +8,19 @@ def incremental_cost(a, b):
 
 
 def fuel_costs(crabs, target, cost_func):
-    return sum([count * cost_func(pos, target) for pos, count in crabs.items()])
+    return sum(count * cost_func(pos, target) for pos, count in crabs.items())
 
 
 def minimum_cost(crabs, cost_func):
     targets = range(min(crabs), max(crabs) + 1)
-    total_costs = [fuel_costs(crabs, target, cost_func) for target in targets]
-
-    return min(total_costs)
+    return min(fuel_costs(crabs, target, cost_func) for target in targets)
 
 
 if __name__ == "__main__":
-    crabs = [int(pos) for pos in open("data.in", "r").read().split(",")]
+    crabs = tuple(map(int, open("data.in", "r").read().split(",")))
 
     # Collect crabs with common position, reduces overhead with ~40%
-    crab_positions = {pos: crabs.count(pos) for pos in crabs}
+    crab_positions = {pos: crabs.count(pos) for pos in set(crabs)}
 
     print(minimum_cost(crab_positions, constant_cost))
     print(minimum_cost(crab_positions, incremental_cost))
