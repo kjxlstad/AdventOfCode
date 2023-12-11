98774a4f0bc15331eaf4315028229e3a9248c5df
kjxlstad <jonathan.kjolstad@gmail.com>
2023-12-11 16:12:19 +0100

    Added day 10 and 11

---
+++ 2023/10/solution.py
@@ -0,0 +1,77 @@
+from itertools import pairwise
+
+N, E, S, W = (0, -1), (+1, 0), (0, +1), (-1, 0)
+
+# (pipe, prev_move): next_move
+MOVES = {
+    ("|", S): S, ("|", N): N,
+    ("-", E): E, ("-", W): W,
+    ("L", W): N, ("L", S): E,
+    ("J", E): N, ("J", S): W,
+    ("7", E): S, ("7", N): W,
+    ("F", W): S, ("F", N): E,
+}
+
+CONNECTIONS = {
+    N: "|7F",
+    S: "|LJ",
+    W: "-FL",
+    E: "-J7",
+}
+
+
+def determine_start_pipe(tiles, x, y):
+    for (dx, dy), pipes in CONNECTIONS.items():
+        nx, ny = x + dx, y + dy
+        if tiles[ny][nx] in pipes:
+            return "-" if dx else "|", (dx, dy)
+
+
+def find_path(tiles, x, y):
+    pipe, (dx, dy) = determine_start_pipe(tiles, x, y)
+    start_x, start_y = x, y
+    path = [(x, y)]
+
+    while True:
+        dx, dy = MOVES[pipe, (dx, dy)]
+        x, y = x + dx, y + dy
+        if (x, y) == (start_x, start_y):
+            break
+        pipe = tiles[y][x]
+        path.append((x, y))
+
+    return path
+
+
+def find_area(path):
+    # Needs to wrap around back to the start
+    path_around = path + [path[0]]
+    return sum(
+        x_0 * y_1 - x_1 * y_0
+        for ((x_0, y_0), (x_1, y_1)) in pairwise(path_around)
+    ) // 2
+
+
+def enclosed_area(area, boundary_length):
+    return area - boundary_length // 2 + 1
+
+
+if __name__ == "__main__":
+    with open("data.in", "r") as f:
+        tiles = f.read().split("\n")
+
+    start_tile = next(
+        (x, y)
+        for y, row in enumerate(tiles)
+        for x, tile in enumerate(row)
+        if tile == "S"
+    )
+
+    path = find_path(tiles, *start_tile)
+    path_length = len(path)
+
+    # Part 1
+    print(path_length // 2)
+
+    # Part 2
+    print(enclosed_area(find_area(path), path_length))
---
+++ 2023/11/solution.py
@@ -0,0 +1,51 @@
+from itertools import combinations
+from functools import partial
+
+
+def find_empty_rows(image):
+    return [i for i, row in enumerate(image) if row == "." * len(row)]
+
+
+def transpose(image):
+    return ["".join(row) for row in zip(*image)]
+
+
+def manhattan_distance(p1, p2):
+    return sum(abs(x - y) for x, y in zip(p1, p2))
+
+
+def galaxy_distance(empty_rows, empty_cols, p1, p2, expansion):
+    min_x, max_x = min(p1[0], p2[0]), max(p1[0], p2[0])
+    min_y, max_y = min(p1[1], p2[1]), max(p1[1], p2[1])
+
+    empty_cols = [x for x in empty_cols if min_x <= x <= max_x]
+    empty_rows = [y for y in empty_rows if min_y <= y <= max_y]
+
+    return (
+        manhattan_distance(p1, p2)
+        + len(empty_cols) * (expansion - 1)
+        + len(empty_rows) * (expansion - 1)
+    )
+
+
+if __name__ == "__main__":
+    with open("data.in", "r") as image:
+        image = image.read().split("\n")
+
+    empty_rows = find_empty_rows(image)
+    empty_cols = find_empty_rows(transpose(image))
+    distance = partial(galaxy_distance, empty_rows, empty_cols)
+
+    galaxies = [
+        (x, y)
+        for y, row in enumerate(image)
+        for x, tile in enumerate(row)
+        if tile == "#"
+    ]
+    pairs = list(combinations(galaxies, 2))
+
+    # Part 1
+    print(sum(distance(p1, p2, expansion=2) for p1, p2 in pairs))
+
+    # Part 2
+    print(sum(distance(p1, p2, expansion=1_000_000) for p1, p2 in pairs))
