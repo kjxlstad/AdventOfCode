9f60c55bb213e4be3333220da17bcd170656c38a
kjxlstad <jonathan.kjolstad@gmail.com>
2024-12-06 19:37:04 +0100

    Clean day 6

--- 2024/06/solution.py
+++ 2024/06/solution.py
@@ -1,43 +1,26 @@
-from dataclasses import dataclass
 from functools import partial
 from multiprocessing import Pool
-from typing import Iterator, Self
+from typing import Iterator
+
+Path = Iterator[tuple[complex, complex]]
 
 
-@dataclass(frozen=True)
-class Vec:
-    x: int
-    y: int
+def patrol(start: complex, w: int, h: int, obstructions: set[complex]) -> Path:
+    pos, vel = start, -1j
 
-    def __add__(self, other: Self) -> Self:
-        return Vec(self.x + other.x, self.y + other.y)
-
-
-def patrol_protocol(pos: Vec, vel: Vec, obstructions: set[Vec]) -> tuple[Vec, Vec]:
-    while (next_pos := pos + vel) in obstructions:
-        vel = Vec(-vel.y, vel.x)
-
-    return next_pos, vel
-
-
-def patrol(
-    pos: Vec, grid_size: Vec, obstructions: set[Vec]
-) -> Iterator[tuple[Vec, Vec]]:
-    vel = Vec(0, -1)
-
-    while 0 <= pos.x < grid_size.x and 0 <= pos.y < grid_size.y:
+    while 0 <= pos.real < w and 0 <= pos.imag < h:
         yield pos, vel
-        pos, vel = patrol_protocol(pos, vel, obstructions)
+
+        while (next_pos := pos + vel) in obstructions:
+            vel *= 1j
+
+        pos = next_pos
 
 
-def guard_visits(pos: Vec, grid_size: Vec, obstructions: set[Vec]) -> set[Vec]:
-    return {pos for pos, _ in patrol(pos, grid_size, obstructions)}
-
-
-def guard_loops(pos: Vec, grid_size: Vec, obstructions: set[Vec]) -> bool:
+def guard_loops(guard_path: Path, obstructions: set[complex]) -> bool:
     visited = set()
 
-    for pos, vel in patrol(pos, grid_size, obstructions):
+    for pos, vel in guard_path(obstructions):
         if (pos, vel) in visited:
             return True
 
@@ -48,17 +31,18 @@ def guard_loops(pos: Vec, grid_size: Vec, obstructions: set[Vec]) -> bool:
 
 if __name__ == "__main__":
     lines = open("data.in").read().splitlines()
-    grid = {Vec(x, y): c for y, line in enumerate(lines) for x, c in enumerate(line)}
+    grid = {x + y * 1j: c for y, line in enumerate(lines) for x, c in enumerate(line)}
 
-    grid_size = Vec(len(lines[0]), len(lines))
+    w, h = len(lines[0]), len(lines)
     obstructions = {pos for pos, char in grid.items() if char == "#"}
     start_pos = next(pos for pos, char in grid.items() if char == "^")
 
     # Part 1
-    visited = guard_visits(start_pos, grid_size, obstructions)
+    guard_path = partial(patrol, start_pos, w, h)
+    visited = {pos for pos, _ in guard_path(obstructions)}
     print(len(visited))
 
     # Part 2
-    obstacles = (obstructions | {pos} for pos in visited if grid[pos] == ".")
-    loops = Pool().map(partial(guard_loops, start_pos, grid_size), obstacles)
+    obstacles = (obstructions | {pos} for pos in visited - {start_pos})
+    loops = Pool().map(partial(guard_loops, guard_path), obstacles)
     print(sum(loops))
